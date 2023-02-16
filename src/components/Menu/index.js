59842898c196be3553e179a4620023fb2e3888a1
dtipton <dylan.tipton@penserv.com>
2023-02-16 10:43:01 -0500

    installed express and sql dependencies

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
-
-const sql = require('sql');
-const app = express();
-var connection = require('express-myconnection');
+//import express from 'express';
+//import bodyParser from 'body-parser';
+//const sql = require('sql');
+//const app = express();
+//var connection = require('express-myconnection');
 
 const Menu = function() {
 
-    app.use(bodyParser.json());
+/*    app.use(bodyParser.json());
 
     connection(sql, {
         host: 'localhost',
@@ -14,21 +15,34 @@ const Menu = function() {
         password: 'password',
         port: 3306,
         database: 'sampledb'
-    })
-
-    String[] months = {
-        "January", "February", "March", "April", "May", "June",
-        "July", "August", "September", "October", "November", "December"};
+    })*/
 
     return (
         <div align="center">
             <h1>Sparky</h1>
 
             <form>
+                <select name="months" id="months">
+                    <option value="jan">January</option>
+                    <option value="feb">February</option>
+                    <option value="mar">March</option>
+                    <option value="apr">April</option>
+                    <option value="may">May</option>
+                    <option value="jun">June</option>
+                    <option value="jul">July</option>
+                    <option value="aug">August</option>
+                    <option value="sept">September</option>
+                    <option value="oct">October</option>
+                    <option value="nov">November</option>
+                    <option value="dec">December</option>
+                </select>
+                <br/>
                 Select a vendor for which to print invoices:
+
                 <select name="vendors" id="vendors">
                     <option value="asdf">asdf</option>
                 </select>
+
             </form>
          </div>
     );
